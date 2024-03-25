fa2854518896d5d9c423722c9f9e84ca9d4f0d55
kiyusama <kiyusama328@gmail.com>
2024-03-25 23:10:53 +0900

    fixed add items to a cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,16 +2,20 @@ class CartItemsController < ApplicationController
     def create
       @cart = current_cart
       @cart_item = @cart.cart_items.find_by(product_id: cart_item_params[:product_id])
+      product = Product.find(params[:product_id])
 
       if @cart_item
         @cart_item.quantity += 1
       else
-        @cart_item = @cart.cart_items.build(cart_item_params)
+        # cart_item_paramsにquantityが含まれていない場合はデフォルト値として1を設定
+        quantity = cart_item_params[:quantity].presence || 1
+        @cart_item = @cart.cart_items.build(product: product, quantity: quantity)
       end
 
       if @cart_item.save
         redirect_to cart_path, notice: "カートに商品を追加しました"
       else
+        Rails.logger.debug @cart_item.errors.full_messages.to_sentence
         redirect_to root_path, alert: "カートに商品を追加できませんでした"
       end
     end
@@ -28,6 +32,6 @@ class CartItemsController < ApplicationController
     end
 
     def cart_item_params
-      params.require(:cart_item).permit(:product_id, :quantity)
+      params.permit(:product_id, :quantity)
     end
 end
